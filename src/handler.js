578e32104dce315dceaa3ac73685dcb18e2f1f70
faiqnoov <mfaiqn@gmail.com>
2023-03-28 00:59:00 +0700

    add query parameters

--- src/handler.js
+++ src/handler.js
@@ -57,16 +57,36 @@ const addBookHandler = (request, h) => {
 }
 
 // GET ALL
-const getAllBooksHandler = () => ({
-  status: 'success',
-  data: {
-    books: books.map((book) => ({
-      id: book.id,
-      name: book.name,
-      publisher: book.publisher,
-    })),
-  },
-});
+const getAllBooksHandler = (request, h) => {
+  const { name, reading, finished } = request.query;
+
+  let filteredBooks = books;
+  
+  if(name !== undefined) {
+    filteredBooks = filteredBooks.filter((book) => book.name.toLowerCase().includes(name.toLowerCase()));
+  }
+
+  if(reading !== undefined) {
+    filteredBooks = filteredBooks.filter((book) => Number(book.reading) === Number(reading));
+  }
+
+  if(finished !== undefined) {
+    filteredBooks = filteredBooks.filter((book) => Number(book.finished) === Number(finished));
+  }
+
+  const response = h.response({
+    status: 'success',
+    data: {
+      books: filteredBooks.map((book) => ({
+        id: book.id,
+        name: book.name,
+        publisher: book.publisher,
+      })),
+    },
+  });
+  response.code(200);
+  return response;
+};
 
 // GET SINGLE BOOK
 const getBookByIdHandler = (request, h) => {
